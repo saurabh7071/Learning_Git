4684814fef4e66c04dd7d3c92aced21c658d597e
Saurabh Yograj Vaidya <saurabhvaidya541@gmail.com>
2023-04-30 10:25:17 +0530

    adding messege in sum.java

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ class sum{
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        System.out.println(a+b);
+        System.out.println("The additon of a and b is "+(a+b));
     }
 }
